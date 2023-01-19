4b2021cb27b88a5fd7a0645b364fdb8f74f460bc
Jordan Lee <jtlee@rmgj2.com>
2023-01-19 10:03:14 -0600

    commit

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,18 @@
 function playSound(animal) {
-    
+    const audio = new Audio(`sounds/${animal}.mp3`)
+    audio.play()
+    console.log(`${animal} clicked!`)
+    selectedAnimalTitle.innerHTML = `The ${animal} goes:`
+
 }
+
+function fillPics(){
+    for (let x = 0; x < animalNames.length; x++) {
+        const animal = animalNames[x];
+        let img = document.createElement('img')
+        document.getElementById(`btn-${animal}`).appendChild(img)
+        img.src = `pictures/${animal}.jpeg`   
+    }
+}
+
+fillPics()
