a3eee3e8a3e32b1334c8392f7280de5abecc6a21
Jordan Lee <jtlee@rmgj2.com>
2023-01-24 09:55:53 -0600

    .

--- scripts/app.js
+++ scripts/app.js
@@ -6,21 +6,24 @@ function playSound(animal) {
     for (let x = 0; x < animalNames.length; x++) {
         if (animal == animalNames[x].name) {
             animalNames[x].isNew = false
-            document.getElementById(`btn-${animal}`).style.backgroundColor = '';
             console.log(animalNames[x].isNew)
         }
+        document.getElementById(`btn-${animalNames[x].name}`).removeChild(document.getElementById(`img-${animalNames[x].name}`))
     }
+    fillPics()
 }
 
 function fillPics(){
     for (let x = 0; x < animalNames.length; x++) {
         const animal = animalNames[x];
         let img = document.createElement('img')
-        document.getElementById(`btn-${animal.name}`).appendChild(img)
+        document.getElementById(`btn-${animal.name}`).appendChild(img).id = `img-${animal.name}`
         img.src = `pictures/${animal.name}.jpeg`  
         if (animal.isNew == true) {
             document.getElementById(`btn-${animal.name}`).style.backgroundColor = 'white';
-        }  
+        } else {
+            document.getElementById(`btn-${animal.name}`).style.backgroundColor = '';
+        }
     }
 }
 
