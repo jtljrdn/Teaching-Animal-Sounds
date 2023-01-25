517666e41a71d59945919baaae24c6517285579e
Jordan Lee <jtlee@rmgj2.com>
2023-01-25 10:36:24 -0600

    fits guidelines for AP

--- scripts/app.js
+++ scripts/app.js
@@ -2,20 +2,23 @@ function playSound(animal) {
     const audio = new Audio(`sounds/${animal}.mp3`)
     audio.play()
     console.log(`${animal} clicked!`)
-    selectedAnimalTitle.innerHTML = `The ${animal} goes:`
     for (let x = 0; x < animalNames.length; x++) {
-        if (animal == animalNames[x].name) {
+        if ((animal == animalNames[x].name) && (animalNames[x].isNew == true)) {
             animalNames[x].isNew = false
             console.log(animalNames[x].isNew)
+            selectedAnimalTitle.innerHTML = `The ${animal} goes:`
+        } else if ((animal == animalNames[x].name) && (animalNames[x].isNew == false)) {
+            selectedAnimalTitle.innerHTML = `Choose a New Animal!`
+            document.getElementById(`btn-${animalNames[x].name}`).disabled = true
         }
         document.getElementById(`btn-${animalNames[x].name}`).removeChild(document.getElementById(`img-${animalNames[x].name}`))
     }
-    fillPics()
+    fillPics(animalNames)
 }
 
-function fillPics(){
-    for (let x = 0; x < animalNames.length; x++) {
-        const animal = animalNames[x];
+function fillPics(array){
+    for (let x = 0; x < array.length; x++) {
+        const animal = array[x];
         let img = document.createElement('img')
         document.getElementById(`btn-${animal.name}`).appendChild(img).id = `img-${animal.name}`
         img.src = `pictures/${animal.name}.jpeg`  
@@ -27,4 +30,3 @@ function fillPics(){
     }
 }
 
-fillPics()
